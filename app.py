084478a43b8df5e4c7c95f8cd1b7f216badb35a3
James Lederman <ledermau@gmail.com>
2019-06-22 11:32:13 -0400

    add config settings

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS']) #sanity check
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+    return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
-	return("Hello {}!".format(name))
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-	app.run()
-
+    app.run()
 
